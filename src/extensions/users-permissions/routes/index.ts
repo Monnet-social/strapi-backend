5247e59eb3468067805f60cc494161cef3a92f53
Ankit Gupta <silverag2002@gmail.com>
2025-06-06 07:38:27 +0530

    feat: add additional authentication routes for user management; include OTP sending, verification, and password reset

--- src/extensions/users-permissions/routes/index.ts
+++ src/extensions/users-permissions/routes/index.ts
@@ -8,4 +8,58 @@ module.exports = [
       auth: false,
     },
   },
+  {
+    method: "POST",
+    path: "/auth/register",
+    handler: "authentication.register",
+    config: {
+      prefix: "",
+      auth: false,
+    },
+  },
+  {
+    method: "GET",
+    path: "/user",
+    handler: "authentication.getUser",
+    config: {
+      prefix: "",
+      auth: false,
+    },
+  },
+  {
+    method: "POST",
+    path: "/auth/register",
+    handler: "authentication.register",
+    config: {
+      prefix: "",
+    },
+  },
+  {
+    method: "POST",
+    path: "/send-otp",
+    handler: "authentication.sendOTP",
+    config: {
+      prefix: "",
+      auth: false,
+    },
+  },
+  {
+    method: "POST",
+    path: "/verify-otp",
+    handler: "authentication.verifyOTP",
+    config: {
+      prefix: "",
+      auth: false,
+    },
+  },
+
+  {
+    method: "POST",
+    path: "/reset-password",
+    handler: "authentication.resetPassword",
+    config: {
+      prefix: "",
+      auth: false,
+    },
+  },
 ];
