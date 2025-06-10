572bd3bc719489947269fc61c91aa71014e18bbf
Ankit Gupta <silverag2002@gmail.com>
2025-06-10 12:13:59 +0530

    fix: update default OTP length in generateOtp method from 4 to 6

--- src/utils/helper_service.ts
+++ src/utils/helper_service.ts
@@ -1,5 +1,5 @@
 export default class HelperService {
-  static generateOtp(length: number = 4) {
+  static generateOtp(length: number = 6) {
     let otp = "";
     for (let i = 0; i < length; i++) {
       otp += Math.floor(Math.random() * 10);
