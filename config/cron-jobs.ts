2d06d0bf87e0e46fa45ffc9003db1e6435de454c
zaid <zaidkamboo100@gmail.com>
2025-06-19 16:08:54 +0530

    Added while loop along with limit for safer deletion of inactive users, added is_email_verified to sent in response of login and also added is_email_verified check in login as well,also overidden apis of posts.

---
+++ config/cron-jobs.ts
@@ -0,0 +1,67 @@
+/**
+ *** CRON FORMAT FOR NO FURTHER CONFUSION !!! ***
+ * | (Minute) | (Hour) | (Day of Month) | (Month) | (Day of Week) |
+ * |    0     |   0    |       *        |    *    |      *        |
+ **/
+
+export default {
+    "0 0 * * *": async ({ strapi }) => {
+        try {
+            const sevenDaysAgo = new Date();
+            sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
+
+            console.log("-------------------------------------------");
+            console.log(`[Cron Job] Running: Delete old, unverified users...`);
+            console.log(
+                `[Cron Job] Deleting users created before: ${sevenDaysAgo.toISOString()}`
+            );
+
+            const batchSize = 100;
+            let usersToDelete;
+            while (true) {
+                usersToDelete = await strapi.entityService.findMany(
+                    "plugin::users-permissions.user",
+                    {
+                        filters: {
+                            createdAt: { $lt: sevenDaysAgo.toISOString() },
+                            is_email_verified: false,
+                        },
+                        fields: ["id", "email"],
+                        limit: batchSize,
+                    }
+                );
+                // LOOP TERMINATION CONDITION...
+                if (usersToDelete.length === 0) {
+                    console.log(
+                        "[Cron Job] No more old, unverified users found."
+                    );
+                    break;
+                }
+
+                console.log(
+                    `[Cron Job] Found a batch of ${usersToDelete.length} user(s) to delete.`
+                );
+
+                const userIdsToDelete = usersToDelete.map((user) => user.id);
+
+                await strapi.entityService.deleteMany(
+                    "plugin::users-permissions.user",
+                    { filters: { id: { $in: userIdsToDelete } } }
+                );
+                for (const user of usersToDelete) {
+                    console.log(
+                        ` -> Deleted user: ${user.email} (ID: ${user.id})`
+                    );
+                }
+            }
+
+            console.log("[Cron Job] Successfully finished cleanup task.");
+            console.log("-------------------------------------------");
+        } catch (error) {
+            console.error(
+                "[Cron Job] Error while deleting old, unverified users:",
+                error
+            );
+        }
+    },
+};
