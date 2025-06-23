8965acae02b8f736ab1475a60a078e764553067c
Ankit Gupta <silverag2002@gmail.com>
2025-06-23 06:33:06 +0530

    feat: add file optimisation API with controller, router, and service

---
+++ src/api/file-optimisation/routes/file-optimisation.ts
@@ -0,0 +1,7 @@
+/**
+ * file-optimisation router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::file-optimisation.file-optimisation');
---
+++ src/api/file-optimisation/services/file-optimisation.ts
@@ -0,0 +1,7 @@
+/**
+ * file-optimisation service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::file-optimisation.file-optimisation');
