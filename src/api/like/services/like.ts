2be94fdb67f41fe673c46a458350bc039198dfb3
Ankit Gupta <silverag2002@gmail.com>
2025-06-24 10:50:25 +0530

    feat: implement like service with methods to get likes count and likes by post ID

--- src/api/like/services/like.ts
+++ src/api/like/services/like.ts
@@ -2,6 +2,26 @@
  * like service
  */
 
-import { factories } from '@strapi/strapi';
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreService('api::like.like');
+export default factories.createCoreService("api::like.like", ({ strapi }) => ({
+  async getLikesCount(postId: number) {
+    const likes = await strapi.db.query("api::like.like").count({
+      where: { post: postId },
+    });
+
+    return likes;
+  },
+  async getLikesByPostId(postId: number) {
+    const likes = await strapi.entityService.findMany("api::like.like", {
+      filters: { post: { id: postId } },
+      populate: {
+        liked_by: {
+          fields: ["id", "username", "email", "name"],
+        },
+      },
+    });
+
+    return likes;
+  },
+}));
