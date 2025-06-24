bd53db2162a7e929f600ea774fee0f32f08ca232
Ankit Gupta <silverag2002@gmail.com>
2025-06-24 10:55:38 +0530

    feat: implement like and unlike functionality in comment controller

--- src/api/comment/controllers/comment.ts
+++ src/api/comment/controllers/comment.ts
@@ -2,6 +2,45 @@
  * comment controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::comment.comment');
+export default factories.createCoreController(
+  "api::comment.comment",
+  ({ strapi }) => ({
+    async likePost(ctx) {
+      const { postId } = ctx.request.body;
+      const userId = ctx.state.user.id;
+      const existingLike = await strapi.entityService.findMany(
+        "api::like.like",
+        {
+          filters: { post: postId, liked_by: userId },
+        }
+      );
+      if (existingLike.length > 0) {
+        return ctx.send("Post liked successfully");
+      }
+      const createLike = await strapi.entityService.create("api::like.like", {
+        data: {
+          post: postId,
+          liked_by: userId,
+        },
+      });
+      return ctx.send("Post liked successfully");
+    },
+    async unlikePost(ctx) {
+      const { postId } = ctx.request.body;
+      const userId = ctx.state.user.id;
+      const existingLike = await strapi.entityService.findMany(
+        "api::like.like",
+        {
+          filters: { post: postId, liked_by: userId },
+        }
+      );
+      if (existingLike.length === 0) {
+        return ctx.send("Post unliked successfully");
+      }
+      await strapi.entityService.delete("api::like.like", existingLike[0].id);
+      return ctx.send("Post unliked successfully");
+    },
+  })
+);
