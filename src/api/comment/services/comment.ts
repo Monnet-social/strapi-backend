3d2905186cb2b3b8c9bb0db5c27ead792364d4ff
Ankit Gupta <silverag2002@gmail.com>
2025-06-24 10:51:34 +0530

    feat: implement comment service with methods to get comments count and comments by post ID

--- src/api/comment/services/comment.ts
+++ src/api/comment/services/comment.ts
@@ -2,6 +2,32 @@
  * comment service
  */
 
-import { factories } from '@strapi/strapi';
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreService('api::comment.comment');
+export default factories.createCoreService(
+  "api::comment.comment",
+  ({ strapi }) => ({
+    async getCommentsCount(postId: number) {
+      const likes = await strapi.db.query("api::comment.comment").count({
+        where: { post: postId },
+      });
+
+      return likes;
+    },
+    async getCommentsByPostId(postId: number) {
+      const likes = await strapi.entityService.findMany(
+        "api::comment.comment",
+        {
+          filters: { post: { id: postId } },
+          populate: {
+            commented_by: {
+              fields: ["id", "username", "email", "name"],
+            },
+          },
+        }
+      );
+
+      return likes;
+    },
+  })
+);
