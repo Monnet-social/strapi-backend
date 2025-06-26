6e85fd159d72175bf7bfb2c5c87ea299f15a9a0e
Ankit Gupta <silverag2002@gmail.com>
2025-06-26 12:03:28 +0530

    feat: update OTP length to 4 and ensure date_of_birth is required in user schema

--- src/utils/helper_service.ts
+++ src/utils/helper_service.ts
@@ -1,39 +1,39 @@
 const shortid = require("shortid");
 
 interface Strapi {
-    entityService: any;
-    query: (uid: string) => any;
+  entityService: any;
+  query: (uid: string) => any;
 }
 
 export default class HelperService {
-    static generateOtp(length: number = 6): string {
-        let otp = "";
-        for (let i = 0; i < length; i++) {
-            otp += Math.floor(Math.random() * 10);
-        }
-        return otp;
+  static generateOtp(length: number = 4): string {
+    let otp = "";
+    for (let i = 0; i < length; i++) {
+      otp += Math.floor(Math.random() * 10);
     }
+    return otp;
+  }
 
-    static async generateUniqueReferralCode(strapi: Strapi): Promise<string> {
-        let referral_code!: string;
-        let isCodeUnique = false;
+  static async generateUniqueReferralCode(strapi: Strapi): Promise<string> {
+    let referral_code!: string;
+    let isCodeUnique = false;
 
-        while (!isCodeUnique) {
-            const candidateCode = shortid.generate();
+    while (!isCodeUnique) {
+      const candidateCode = shortid.generate();
 
-            const usersWithCode = await strapi.entityService.findMany(
-                "plugin::users-permissions.user",
-                {
-                    filters: { referral_code: candidateCode },
-                    fields: ["id"],
-                }
-            );
-
-            if (usersWithCode.length === 0) {
-                referral_code = candidateCode;
-                isCodeUnique = true;
-            }
+      const usersWithCode = await strapi.entityService.findMany(
+        "plugin::users-permissions.user",
+        {
+          filters: { referral_code: candidateCode },
+          fields: ["id"],
         }
-        return referral_code;
+      );
+
+      if (usersWithCode.length === 0) {
+        referral_code = candidateCode;
+        isCodeUnique = true;
+      }
     }
+    return referral_code;
+  }
 }
