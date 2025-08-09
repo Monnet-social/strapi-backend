4496541ee562dac420f73b42bb685937f7a7cb0f
Ankit Gupta <silverag2002@gmail.com>
2025-08-09 19:52:37 +0530

    Refactor algorithm control update logic and improve error handling

--- src/api/algorithm-control/controllers/algorithm-control.ts
+++ src/api/algorithm-control/controllers/algorithm-control.ts
@@ -14,8 +14,8 @@ export default factories.createCoreController(
     },
 
     async update(ctx) {
-      const { documentId: userId } = ctx.state.user;
-      const { controlDocumentId } = ctx.params;
+      const { id: userId } = ctx.state.user;
+
       // {
       //   friends:0-100,
       //   followings:0-100,
@@ -28,29 +28,61 @@ export default factories.createCoreController(
       //     },
       //   ],
       // }
+
+      const alog_control = await strapi
+        .service("api::algorithm-control.algorithm-control")
+        .findOrCreate(userId);
+      if (!alog_control.id) {
+        return ctx.badRequest("Control document not found for user.");
+      }
       console.log("Updating algorithm control for user:", userId);
       const data = ctx.request.body;
       let control: any = {};
 
-      if (!data.friends) control.friends = data.friends;
+      if (data.friends) control.friends = data.friends;
 
-      if (!data.followings) control.followings = data.followings;
+      if (data.followings) control.followings = data.followings;
 
-      if (!data.recommendations) control.recommendations = data.recommendations;
+      if (data.recommendations) control.recommendations = data.recommendations;
 
-      if (!data.distance) control.distance = data.distance;
+      if (data.distance) control.distance = data.distance;
 
-      if (data.categories_entry)
-        control.categories_entry = data.categories_entry.map((entry) => ({
-          category: { id: entry.category.id },
-          weight: entry.weight,
-        }));
+      if (data.categories_entry) {
+        //get old category and new category map with category id and then remove id from component and create new category with updated data and create new one
+        let categories = [];
+        for (const entry of alog_control.categories_entry) {
+          console.log("Processing category entry:", entry);
+          const updatedCategory = data.categories_entry.find(
+            (cat) => cat.category.id === entry.category.id
+          );
+          if (updatedCategory) {
+            // If the category exists, update its weight
+            categories.push({
+              category: updatedCategory.category.id,
+              weightage: updatedCategory.weight,
+            });
+          } else {
+            // If the category does not exist, get old category and push them by removing id
+            categories.push({
+              category: entry.category.id,
+              weightage: entry.weightage,
+            });
+          }
+        }
+        control.categories_entry = categories;
+      }
 
-      const response = await strapi
-        .documents("api::algorithm-control.algorithm-control")
-        .update({
-          documentId: controlDocumentId,
-          filters: { user: { documentId: userId } },
+      console.log(
+        "Updating control document with ID:",
+        control.categories_entry
+      );
+
+      console.log("Updating control document with ID:", control);
+
+      const response = await strapi.entityService.update(
+        "api::algorithm-control.algorithm-control",
+        alog_control.id,
+        {
           data: control,
           populate: {
             categories_entry: {
@@ -59,7 +91,8 @@ export default factories.createCoreController(
               },
             },
           },
-        });
+        }
+      );
 
       return ctx.send({
         message: "Algorithm control updated successfully",
