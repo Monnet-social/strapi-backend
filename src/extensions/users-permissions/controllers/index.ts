cba99d8434166d38641b0ee528e87790a93c48b6
Ankit Gupta <silverag2002@gmail.com>
2025-08-11 17:15:24 +0530

    Add Mesibo integration and update environment configuration

--- src/extensions/users-permissions/controllers/index.ts
+++ src/extensions/users-permissions/controllers/index.ts
@@ -1,7 +1,9 @@
 const authentication = require("./authentication");
 const profile = require("./profile");
+const mesibo = require("./mesibo");
 
 module.exports = {
-    authentication,
-    profile,
+  authentication,
+  profile,
+  mesibo,
 };
