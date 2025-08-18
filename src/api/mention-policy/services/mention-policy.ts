a94b21554316ce540aaf0d860e4037d01e9a2396
zaid <zaidkamboo100@gmail.com>
2025-08-18 16:18:27 +0530

    Fixed crate post

--- src/api/mention-policy/services/mention-policy.ts
+++ src/api/mention-policy/services/mention-policy.ts
@@ -6,16 +6,10 @@ export default factories.createCoreService(
     async findOrCreateMentionPolicy(userId) {
       const findMentionPolicy = await strapi.entityService.findMany(
         "api::mention-policy.mention-policy",
-        {
-          filters: {
-            user: userId,
-          },
-        }
+        { filters: { user: userId } }
       );
 
-      if (findMentionPolicy.length > 0) {
-        return findMentionPolicy[0];
-      }
+      if (findMentionPolicy.length > 0) return findMentionPolicy[0];
 
       return await strapi.entityService.create(
         "api::mention-policy.mention-policy",
@@ -35,17 +29,14 @@ export default factories.createCoreService(
       content: string,
       type: "story" | "comment" | "post"
     ) {
-      if (typeof content !== "string") {
+      if (typeof content !== "string")
         throw new TypeError("content must be a string");
-      }
 
       let finalMention = [];
 
       for (let i = 0; i < content.length; i++) {
         if (content[i] === "@") {
           let allowTagging = false;
-
-          // Extract the username following the @ symbol
           const restOfContent = content.slice(i + 1);
           const username = restOfContent.split(" ")[0] || "";
 
@@ -54,11 +45,7 @@ export default factories.createCoreService(
           const findUser = await strapi.entityService.findMany(
             "plugin::users-permissions.user",
             {
-              filters: {
-                username: {
-                  $eq: username,
-                },
-              },
+              filters: { username: { $eq: username } },
               fields: ["id"],
               limit: 1,
             }
@@ -71,24 +58,17 @@ export default factories.createCoreService(
           );
 
           let policy = "anyone";
-          if (type === "story") {
-            policy = findMentionPolicy.story_policy;
-          } else if (type === "comment") {
+          if (type === "story") policy = findMentionPolicy.story_policy;
+          else if (type === "comment")
             policy = findMentionPolicy.comment_policy;
-          } else if (type === "post") {
-            policy = findMentionPolicy.post_policy;
-          }
+          else if (type === "post") policy = findMentionPolicy.post_policy;
 
           const userFollowers = await strapi.entityService.findMany(
             "api::following.following",
             {
               filters: {
-                subject: {
-                  id: Number(findUser[0].id),
-                },
-                follower: {
-                  id: Number(userId),
-                },
+                subject: { id: Number(findUser[0].id) },
+                follower: { id: Number(userId) },
               },
               fields: ["id"],
               limit: 1,
@@ -99,12 +79,8 @@ export default factories.createCoreService(
             "api::following.following",
             {
               filters: {
-                follower: {
-                  id: Number(findUser[0].id),
-                },
-                subject: {
-                  id: Number(userId),
-                },
+                follower: { id: Number(findUser[0].id) },
+                subject: { id: Number(userId) },
                 is_close_friend: true,
               },
               fields: ["id"],
@@ -120,9 +96,8 @@ export default factories.createCoreService(
               )) ||
             (policy === "followers" &&
               userFollowers.some((follower) => follower.id === findUser[0].id))
-          ) {
+          )
             allowTagging = true;
-          }
 
           finalMention.push({
             user: findUser[0].id,
